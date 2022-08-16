0be27087ed523a78e7227de8e96e3f2aa92d1a94
HannaMin8 <anyamin1984@gmail.com>
2022-08-16 19:35:24 +0200

    Added the forecast JS and API

--- src/index.js
+++ src/index.js
@@ -21,6 +21,37 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class = "row">`;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+	<div class="col-2">
+		<div class="weather-forecast-date">${day}</div>
+		<i class="fa-solid fa-sun"></i>
+		<div class="weather-forecast-temperatures">
+			<span class="weather-forecast-temperature-max">18°C</span>
+			<span class="weather-forecast-temperature-min">22°C</span>
+		</div>
+	</div>
+	`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "17e6938b8ca35f2e9d6cfa919b2079c1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showData(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temp");
@@ -46,6 +77,8 @@ function showData(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   console.log(response);
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(event) {
